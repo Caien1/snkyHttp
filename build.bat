b73e0dc1351f7b10e4b72e3ec5e691e1e415f0e7
Caien1 <Caien1>
2025-09-19 17:10:50 +0530

    build

--- build.bat
+++ build.bat
@@ -1,2 +1,26 @@
-mkdir bin
-g++ .\src\libcurlWrapper.cpp -o .\bin\test.exe -I.\src -lcurl -Wall -lssl -lcrypto -lz -lws2_32 -lcrypt32 -lwldap32  -DCURL_STATICLIB
+
+REM ADD A BIN DIR BEFORE RUNNING
+
+@echo off
+
+echo Compiling Curl...
+set cc=g++
+set source=.\src\libcurlWrapper.cpp
+set output=-o .\bin\test.exe
+set flag=-I .\src -lcurl -Wall -lssl -lcrypto -lz -lws2_32 -lcrypt32 -lwldap32 -DCURL_STATICLIB
+
+%cc% %source% %output% %flag%
+
+echo Done
+
+
+echo Compiling Raylib...
+set sourceui=.\src\ui.cpp 
+set outputui=-o .\bin\ray.exe
+set flagui=-I include -L lib -lraylib -lgdi32 -lwinmm -Wall
+
+%cc% %sourceui% %outputui% %flagui%
+
+echo Done
+pause
+
