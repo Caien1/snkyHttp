7116fbd5defa3752798edd220544f65738666dac
Caien1 <Caien1>
2025-09-19 17:10:02 +0530

    driver code

--- src/libcurlWrapper.cpp
+++ src/libcurlWrapper.cpp
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include "libcurlWrapper.h"
+
  
 
 int main(void)
 {
 
 Memory mem={0};
-RequestHandle *Handle;
-HeaderList *list;
-CurlIntialiseGlobal();
+RequestHandle *Handle=NULL;
+HeaderList *list=NULL;
 
+CurlInitialiseGlobal();
 
-  GetObject Obj;
-  Obj.url="https://www.purgomalum.com/service/json?text=this";
+  GetReqObject Obj;
+  Obj.url="https://google.com/";
   Obj.redirect=0;
 
 
 Handle = RequestHandleInit(Handle);
+
 Handle = GetSetter(Handle,&Obj);
-Handle = WriteToMemory(Handle, (void*)&mem);
-VerboseOutput(Handle);
+list = AddHeader(list,"Hello: Test");
+list = AddHeader(list,"Param2: Test");
+list = AddHeader(list,"Param3: Test");
+Handle = SetCustomHeaders(Handle,list);
+
+
+Handle = SetCustomHeaders(Handle, list);
+
 ResponseCode response = SendRequest(Handle);
 
+if(response==CURLE_OK){
+  printf("Okay\n");
+}
+
+
+    
+       
+       
 
 
 RequestHandleCleaner(Handle);
 CurlDestroyGlobal();
 
-printf("%s", mem.response);
 
   return 0;
 }
