c4f12d7d8ad23e24d78b3ec227e708724c378965
Caien1 <Caien1>
2025-09-23 23:32:39 +0530

    Too lazy to commit

--- src/uiDriver.cpp
+++ src/uiDriver.cpp
@@ -1,24 +1,30 @@
 #include<raylib.h>
 #include<raygui.h>
 #include "uiImpl.h"
+#define URL_BUFFER 1024
+
+
 int main(){
+
+char buffer[URL_BUFFER +1] ="\0";
 InitWindow(800,800,"Http Status");
 
 Color fav= {51,51,51,0};
-Rectangle rect ={0,0,10,10};
+// Rectangle rect ={0,0,10,10};
 
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())    
     {
         // TODO: Update your variables here
 
-DrawText(TextFormat("Score: %08i"), 400, 400, 50, RED);
             BeginDrawing();
 
-            InsideLoop();
+        //  InsideLoop();
+        //  if(IsKeyPressed(KEY_F)) ToggleFullScreenWindow();
             
             ClearBackground(fav);
-             
+            DrawTextOnScreen();
 
 
         EndDrawing();
--- src/uiImpl.h
+++ src/uiImpl.h
@@ -1,6 +1,30 @@
 #include<raylib.h>
-void InsideLoop(){
-    int score = 50;
+void InsideLoop();
+void ToggleFullScreenWindow();
 
-            DrawFPS(500,500);
+void InsideLoop(){
+    
+    DrawFPS(500,500);
+    
+}
+
+void ToggleFullScreenWindow(){
+
+    if(!IsWindowFullscreen()){
+    int display = GetCurrentMonitor();
+
+        SetWindowSize(GetMonitorWidth(display),GetMonitorHeight(display));
+        ToggleFullscreen();
+
+    }else{
+
+                SetWindowSize(800,800);
+                ToggleFullscreen();
+    }
+
+}
+
+void DrawTextOnScreen(){
+     DrawRectangle(400, 400, 40, 5, RAYWHITE);
+     
 }
