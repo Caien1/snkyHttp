8dd999e97e33a23d1af8ecf7f34726343444667f
Caien1 <pulwama01@gmail.com>
2025-10-01 17:46:52 +0530

    build file

--- build.bat
+++ build.bat
@@ -1,25 +1,27 @@
 
 @echo off
 REM ADD A BIN DIR BEFORE RUNNING
-
-
-echo Compiling Curl...
 set cc=g++
-set source=.\src\libcurlWrapper.cpp
-set output=-o .\bin\test.exe
-set flag=-I .\src -lcurl -Wall -lssl -lcrypto -lz -lws2_32 -lcrypt32 -lwldap32 -DCURL_STATICLIB
 
-%cc% %source% %output% %flag%
+@REM echo Compiling Curl...
 
-echo Done
+@REM set source=.\src\libcurlWrapper.cpp
+@REM set output=-o .\bin\test.exe
+@REM set flag=-I .\src -lcurl -Wall -lssl -lcrypto -lz -lws2_32 -lcrypt32 -lwldap32 -DCURL_STATICLIB
+
+@REM %cc% %source% %output% %flag%
+
+@REM echo Done
 
 
 echo Compiling Raylib...
 set sourceui=.\src\uiDriver.cpp 
 set outputui=-o .\bin\ray.exe
-set flagui=-I include -L lib -lraylib -lgdi32 -lwinmm -Wall
+set flagui=-I include -L lib -lraylib  -lgdi32 -lwinmm -Wall -L./ -lui
 
 %cc% %sourceui% %outputui% %flagui%
 
 echo Done
 
+g++ -c .\src\uiImpl.cpp -o ./build/ui.o -L lib -lraylib  -lgdi32 -lwinmm -Wall
+g++ -shared -o ./bin/ui.dll ./build/ui.o  -L lib -lraylib  -lgdi32 -lwinmm -Wall
