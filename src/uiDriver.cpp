20daf361a56651822b9cdc2f243b87ec6526cf86
Caien1 <pulwama01@gmail.com>
2025-10-01 17:47:27 +0530

    UI stuff

--- src/uiDriver.cpp
+++ src/uiDriver.cpp
@@ -1,5 +1,9 @@
 #include<raylib.h>
 #include "uiImpl.h"
+#define RAYGUI_IMPLEMENTATION
+
+#include"../include/raygui.h"
+
 #define URL_BUFFER 1024
 
 
@@ -15,15 +19,21 @@ Color fav= {51,51,51,0};
 
     while (!WindowShouldClose())    
     {
+        
         // TODO: Update your variables here
 
             BeginDrawing();
 
-        //  InsideLoop();
+
+        InsideLoop();
         //  if(IsKeyPressed(KEY_F)) ToggleFullScreenWindow();
-            
+        bool test = true;
+            if(GuiTextInputBox((Rectangle){200,200,200, 200},"None","test" ,"try","sdfs",15,&test)){test = !test;}
+    //        GuiButton((Rectangle){200,200,200, 15},"send");
             ClearBackground(fav);
-            DrawTextOnScreen();
+
+
+        
 
 
         EndDrawing();
