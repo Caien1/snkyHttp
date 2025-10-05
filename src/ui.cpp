40aa836aa3e2cd0c8e3d93bdaa7eec315b209de3
Caien1 <pulwama01@gmail.com>
2025-10-05 23:11:23 +0530

    fonts and gui

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,72 +1,95 @@
-    // optional: only if you don’t draw shapes
 #include <raylib.h>
 
+
 #define RAYGUI_IMPLEMENTATION
-
 #include "../include/raygui.h"
+#include <stdio.h>
+#define BACKGROUND_COLOR  (Color){59,59,59,255}
+#define SIDE_VIEW_COLOR (Color){23,23,23,255}
+#define SCALING_FACTOR 1.4
+#define URL_BUFFER 256
 
-#define URL_SIZE 256
-
-namespace curl{
-#include "libcurlWrapper.h"
+void draw_history_section(){
+  Rectangle  history_view = {0,0,GetScreenWidth()*0.3,GetScreenHeight()};
+  DrawRectangleRec(history_view,SIDE_VIEW_COLOR);
 }
-#define _WIN32 1
+
+void draw_request_maker(bool *drop_down_active, int *current_drop_down_active,char *buffer){
+ 
+  
+  int x = int(GetScreenWidth()*0.5) ;
+  int y = 20;
+  int height = 60;
+  int width = 60;
+  
+
+  Rectangle drop_down = {x,y,width*1.5,height};
+  Rectangle input_box = {x+drop_down.width,y,width*10,height};
+  Rectangle btn = {input_box.width+input_box.x,y,width*2,height};
+
+  GuiButton(btn,"SEND");
+  GuiSetStyle(DEFAULT, BORDER_COLOR_PRESSED, 0x0492c7ff);
+  GuiSetStyle(DEFAULT, BASE_COLOR_PRESSED, 0X33333333);
+  GuiSetStyle(DEFAULT, TEXT_COLOR_PRESSED, 0x000000ff);
+  
+  GuiSetStyle(DEFAULT, BASE_COLOR_FOCUSED, 0xffffffff);
+  GuiSetStyle(DEFAULT, TEXT_COLOR_NORMAL, 0x000000ff);
+
+ if (GuiDropdownBox(drop_down,"GET\nPOST\nDEL",current_drop_down_active,*drop_down_active)){
+  *drop_down_active = !*drop_down_active;
+ }
+
+    
+  GuiSetStyle(DEFAULT, BORDER_COLOR_PRESSED, 0x0492c7ff);
+  GuiSetStyle(DEFAULT, TEXT_COLOR_PRESSED, 0xffffffffff);
+  DrawRectangleRec(input_box,SIDE_VIEW_COLOR);
+   
+  GuiTextBox(input_box,buffer,250,true);
+
+
+}
 
 int main(void) {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  const int screenWidth = 1024;
-  const int screenHeight = 720;
 
   /*_____________________________________________Curl Init_____________________________________________*/
-curl::Memory mem={0};
-curl::RequestHandle *Handle=NULL;
-curl::HeaderList *list=NULL;
-curl::CurlInitialiseGlobal();
 
-/*_____________________________________________Curl Init_____________________________________________*/
 
-  InitWindow(screenWidth, screenHeight, "sneakyHttp");
+/*_____________________________________________Curl_____________________________________________*/
 
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
+/*__________________________________________SORT OF WI________________________________________________________*/
 
-    char buffer[URL_SIZE+1]="https://";
-    Rectangle textBox = {10,10,screenWidth/2,35};
+const int screenHeight  = 720;
+const int screenWidth = 1080;
 
-    Rectangle btn = {textBox.x+textBox.width, textBox.y,50,35 };
-    Rectangle messageBox = {10,50,screenWidth/2+50,screenHeight/2};
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);    
+  InitWindow(screenWidth, screenHeight, "SneakyHttp");
+  SetTargetFPS(60);
 
-    curl::GetReqObject ent;
-  // Main game loop
+  bool is_active = false;
+  int  current_drop_down_active=0;
+  char buffer[URL_BUFFER];
+
+    Font font = LoadFontEx("C:\\Users\\pulwa\\OneDrive\\Desktop\\discoe\\RequestMaker\\snkyHttp\\src\\ubuntu.ttf",50,NULL,0);
+    GuiSetFont(font);
+ 
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
-
+    int currentWidth = GetScreenWidth();
+    int currentHeight = GetScreenHeight();
      BeginDrawing();
 
-      GuiTextBox(textBox,buffer,URL_SIZE+1,true);
-      if(GuiButton(btn,"Send")){
-        mem.size=0;
-        ent.url = buffer;
-        printf("%s",ent.url);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 20);                // DEFAULT, shared by all controls
 
-        Handle = curl::RequestHandleInit(Handle);
-        Handle = curl::GetSetter(Handle,&ent);
-        curl::WriteToMemory(Handle,(void*)&mem);
-        curl::ResponseCode response = curl::SendRequest(Handle);
-        if(response==curl::CURLE_OK){
-}
-
-
-
-      }
-      GuiMessageBox(messageBox,"data",mem.response,"");
-
-       ClearBackground( GRAY);
+    GuiSetStyle(DEFAULT, BORDER_WIDTH, 0);
+     draw_history_section();
+   draw_request_maker(&is_active,&current_drop_down_active, buffer);
     
+     
+      
 
+      EndDrawing();
+      ClearBackground(BACKGROUND_COLOR);
 
-     EndDrawing();
   }
 
   //--------------------------------------------------------------------------------------
