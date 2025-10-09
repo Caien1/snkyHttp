464f0060344987cfd557cbcb6a562ff1859e1fd6
Caien1 <pulwama01@gmail.com>
2025-10-09 14:14:17 +0530

    some components

---
+++ src/uiComponents.cpp
@@ -0,0 +1,36 @@
+#define RAYGUI_IMPLEMENTATION
+#include "../include/raygui.h"
+
+#include <stdio.h>
+
+typedef struct drop_down{
+    Rectangle coords;
+    char * items;
+    int active;
+    bool edit;
+}DropDown;
+
+
+
+
+
+void drop_down_widget(DropDown mem){
+
+
+
+
+
+
+}
+
+
+void text_box(){
+
+
+}
+
+void scroll_response(){
+
+
+    
+}
