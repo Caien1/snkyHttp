53dd1c00956b707182f90359b68d1625216763de
Caien1 <pulwama01@gmail.com>
2025-10-09 14:50:44 +0530

    some components

--- src/uiComponents.cpp
+++ src/uiComponents.cpp
@@ -9,27 +9,29 @@ typedef struct drop_down{
     int active;
     bool edit;
 }DropDown;
-
-
-
-
-
-void drop_down_widget(DropDown mem){
-
-
-
-
-
+void drop_down_widget(DropDown* dropDown){
+if(GuiDropdownBox(dropDown->coords,dropDown->items,&dropDown->active,dropDown->edit)){
+    dropDown->edit= !dropDown->edit;
+    }
 
 }
 
-
-void text_box(){
-
-
+typedef struct text_box{
+    Rectangle coords;
+    char* url;
+    int size;
+    bool edit;
+}TextBox;
+void text_box_widget(TextBox * textBox,bool *flag){
+textBox->edit = true;
+if(GuiTextBox(textBox->coords,textBox->url,textBox->size,textBox->edit)){
+  *flag = true;
 }
+}
+
 
 void scroll_response(){
+    
 
 
     
